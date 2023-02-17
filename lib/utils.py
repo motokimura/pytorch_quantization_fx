e1021d6658aaff8333ab32a184bb343bbf1c7622
Motoki Kimura <motoki.kimura.1990@gmail.com>
2023-02-17 18:23:17 +0900

    add a notebook

--- lib/utils.py
+++ lib/utils.py
@@ -82,14 +82,14 @@ def prepare_dataloaders(batch_size):
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]
     )
-    train_dataset = CIFAR10(root="./data", train=True, download=True, transform=train_transform)
+    train_dataset = CIFAR10(root="~/data", train=True, download=True, transform=train_transform)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
     # prepare test dataloader
     test_transform = transforms.Compose(
         [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
     )
-    test_dataset = CIFAR10(root="./data", train=False, download=True, transform=test_transform)
+    test_dataset = CIFAR10(root="~/data", train=False, download=True, transform=test_transform)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
 
     return train_dataloader, test_dataloader
